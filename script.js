c701ae23be1f10808f8446d3902dfb566b8c7ffd
abng01 <alicia.bang404@gmail.com>
2025-07-16 14:20:39 +1000

    changes made to IOD folder

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!")
+
+function sayHello(){
+    console.log("Hello World!")
+}
+
+sayHello()
+
+function sum(a, b){
+    console.log(a + b)
+}
+
+sum(2, 3)
+
+function returnSum(a, b){
+    return a - b
+}
+
+console.log(returnSum(2, 3))
